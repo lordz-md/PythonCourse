3ec1802367a39a2eb6108a4cb3ce3da79d87371b
Traian Ciobanu <traian.ciobanu@toyotaconnected.eu>
2021-05-05 17:22:21 +0000

    python shit

---
+++ class.py
@@ -0,0 +1,9 @@
+class Fruits:
+    def __init__(self, fruit, color):
+        self.fruit = fruit
+        self.color = color
+
+#Init class
+fruit = Fruits('Bannana', 'Yellow')
+
+print(fruit.color)
