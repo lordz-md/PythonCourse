07d6fa19f73dd17355501ca486b820fa1c61b48a
Traian Ciobanu <traian.ciobanu@toyotaconnected.eu>
2021-05-05 16:52:37 +0000

    more random python shit

---
+++ basic_if_statement.py
@@ -0,0 +1,13 @@
+name = 'John'
+name = 'Betty'
+#name = 'Sue'
+
+if name == 'Mike':
+    print('correct')
+
+elif name == 'Betty':
+    print('My name is not Betty!')
+
+
+else:
+    print('Mike was not found')
